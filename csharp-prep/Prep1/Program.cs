10e1c80c63c2abf1e9dbc7c27363a3a4e4477bf7
charlesofuasia <ofu23001@byui.edu>
2023-04-18 09:41:50 +0100

    Completed prep1 task

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        string FirstName = Console.ReadLine();
+        Console.Write("What is your surname? ");
+        string Surname = Console.ReadLine();
+        //Use char function and Toupper method to capitalize output in cases where user input are in lower cases
+        Console.WriteLine($"Your name is {char.ToUpper(Surname[0]) + Surname.Substring(1)},  {char.ToUpper(FirstName[0]) + FirstName.Substring(1)} {char.ToUpper(Surname[0]) + Surname.Substring(1)}.");
+       
     }
 }
