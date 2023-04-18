7b9f2449e234cd0410c1eda7450387e6be2076e4
charlesofuasia <ofu23001@byui.edu>
2023-04-18 13:41:06 +0100

    prep 2 completed with stretch attempt

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,83 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+      // to classify a given score into a grade
+      //get raw score input from user
+      Console.Write("Enter a score: ");
+      string score = Console.ReadLine();
+      //convert string input to an integer
+      int scoreNumber = int.Parse(score);
+
+      if (scoreNumber >= 90)
+      {
+        if (scoreNumber % 10 >= 3 || scoreNumber >= 100)
+        {
+            Console.WriteLine("Grade is A");
+        }
+        else
+        {
+            Console.WriteLine("Grade is A-");
+        }
+      }
+      else if (scoreNumber >= 80)
+      {
+        if (scoreNumber % 10 >= 7)
+        {
+            Console.WriteLine("Grade is B+");
+        }
+        else if (scoreNumber % 10 < 3)
+        {
+            Console.WriteLine("Grade is B-");
+        }
+        else
+        {
+            Console.WriteLine("Grade is B");
+        }
+      }
+      else if (scoreNumber >= 70)
+      {
+        if (scoreNumber % 10 >= 10)
+        {
+            Console.WriteLine("Grade is C+");
+        }
+        else if (scoreNumber % 10 < 3)
+        {
+            Console.WriteLine("Grade is C-");
+        }
+        else
+        {
+             Console.WriteLine("Grade is C");
+        }
+       
+      }
+      else if (scoreNumber >= 60)
+      {
+        if (scoreNumber % 10 >= 7)
+        {
+            Console.WriteLine("Grade is D+");
+        }
+        else if (scoreNumber % 10 < 3)
+        {
+            Console.WriteLine("Grade is D-");
+        }
+        else
+        {
+            Console.WriteLine("Grade is D");
+        }
+      }
+      else
+      {
+        Console.WriteLine("Grade is F");
+      }
+      if (scoreNumber >= 70)
+      {
+        Console.WriteLine("You passed!");
+      }
+      else
+      {
+        Console.WriteLine("Sorry you did not make it.");
+      }
+
+
     }
 }
