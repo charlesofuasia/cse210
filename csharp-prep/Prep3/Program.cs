56dd2e521b1115f6f7677c341e9c55fcc09ce543
charlesofuasia <ofu23001@byui.edu>
2023-04-21 09:13:11 +0100

    Prep 3 task with one stretch done

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -2,8 +2,40 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep3 World!");
+    static void Main(string[] arg)
+    { 
+        int guessNum = 0;
+
+        string playOn = "yes";
+       
+       while (playOn == "yes")
+       {
+        Random randomGenerator = new Random();
+        int magicNum = randomGenerator.Next(1, 100);
+
+          do
+        {
+           Console.Write("Guess the magic number: ");
+           string guess = Console.ReadLine();
+           guessNum = int.Parse(guess);
+
+           if (magicNum > guessNum)
+           {
+            Console.WriteLine("Higher.");
+           }
+           else if (magicNum < guessNum)
+           {
+            Console.WriteLine("Lower.");
+           }
+           else
+           {
+            Console.WriteLine("You guessed the magic number correctly.");
+           }
+
+        } while (guessNum != magicNum);
+
+        Console.Write("Would you like to continue playing? yes/no: ");
+        playOn = Console.ReadLine();
+       }
     }
 }
