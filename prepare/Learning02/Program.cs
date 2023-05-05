e7dce0ff66bb876951e4b07456fa87f969a803d6
Charles Ofuasia <ofu23001@byui.edu>
2023-05-05 14:39:59 +0100

    Edited comments in Program.cs and Resume.cs

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,23 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Job job1 = new Job();
-        job1._companyName = "Microsoft";
-        job1._jobTitle = "Software Engineer";
-        job1._startYear = 2019;
-        job1._endYear = 2022;
+        Job job1 = new Job(); //new job job1 is created by calling the job class.
+        job1._companyName = "Microsoft";  //name attribute assigned to job1
+        job1._jobTitle = "Software Engineer"; // job title attribute created for job1
+        job1._startYear = 2019; //start year attribute assigned
+        job1._endYear = 2022; // end year attribute assigned for job1
 
-        Job job2 = new Job();
+        Job job2 = new Job();// another job created job2 and similar attributes as job1 assigned
         job2._companyName = "Apple";
         job2._jobTitle = "Manager";
         job2._startYear = 2022;
         job2._endYear = 2023;
 
 
-        Resume myResume = new Resume();
+        Resume myResume = new Resume();   //a new resume object is created
         myResume._name = "Allison Rose";
-        myResume._jobs.Add(job1);
-        myResume._jobs.Add(job2);
+        myResume._jobs.Add(job1);  // job1 added to the _job list
+        myResume._jobs.Add(job2); // job2 also added.
 
         myResume.ShowJobs();
 
--- prepare/Learning02/Resume.cs
+++ prepare/Learning02/Resume.cs
@@ -1,10 +1,14 @@
 using System;
 
-public class Resume
+public class Resume    // New class Resume created.
 {
-  public string _name;
-  public List<Job> _jobs = new List<Job>();
-
+  // attributes or variables which are required for Resume class perform its tasks
+  public string _name;  //string variable for user's name
+  public List<Job> _jobs = new List<Job>(); // list variable of type Job created. This means that
+                                            // the Job class created in Job.cs is a data type contained
+                                            // in the variable/attribute, _job 
+  
+  // function or declaration of task for Resume class
   public void ShowJobs()
   {
     Console.WriteLine($"Name: {_name}");
