70dc8dc4cb1e7ea1b1c9d32eeb4209f89ef23ab7
Charles Ofuasia <ofu23001@byui.edu>
2023-05-05 14:13:26 +0100

    Added comments to Job.cs file

--- prepare/Learning02/Job.cs
+++ prepare/Learning02/Job.cs
@@ -1,13 +1,15 @@
 using System;
-
+//declare a new class Job
 public class Job
 
 {
+    //the attributes or variables that Job class will require to perform its roles
     public string _companyName;
     public string _jobTitle;
     public int _startYear;
     public int _endYear;
 
+// function declaration or task performed by the Job class.
     public void DisplayFullDetails()
     {
         Console.WriteLine($"{_jobTitle} ({_companyName}) {_startYear} - {_endYear}");
