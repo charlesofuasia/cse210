ab4b9daedd4f60c3ab7e82b7765e5b66b7410b2b
Charles Ofuasia <ofu23001@byui.edu>
2023-05-07 21:51:05 +0100

    Deleted Hello World! from Program.cs

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+    
         PromptGenerator _prompt = new PromptGenerator();
         _prompt._dayLessonPrompt = "What is the most important lesson you learnt today?";
 
