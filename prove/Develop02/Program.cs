d2e09b7051af7d9d35d29ffbec2971711e75e693
Charles Ofuasia <ofu23001@byui.edu>
2023-05-07 21:47:30 +0100

    PromptGenerator class file created

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -5,5 +5,9 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Develop02 World!");
+        PromptGenerator _prompt = new PromptGenerator();
+        _prompt._dayLessonPrompt = "What is the most important lesson you learnt today?";
+
+        Console.WriteLine(_prompt._dayLessonPrompt);
     }
 }
---
+++ prove/Develop02/PromptGenerator.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class PromptGenerator
+{
+    // The following are the attributes for the PromptGenerator class
+    public string _scripturePrompt;
+    public string _improvementPrompt;
+    public string _remarkableEventPrompt;
+    public string _remarkablePersonprompt;
+    public string _dayTasksPrompt;
+    public string _dayLessonPrompt;
+
+    public void GenerateRandomPrompt()
+    {
+
+    }
+}
