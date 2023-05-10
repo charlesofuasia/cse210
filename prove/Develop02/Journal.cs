727a53327c534191c77a7f616a591711405fdc67
Charles Ofuasia <ofu23001@byui.edu>
2023-05-10 17:37:00 +0100

    Created Journal.cs file

---
+++ prove/Develop02/Journal.cs
@@ -0,0 +1,24 @@
+using System;
+
+public class Journal
+{
+    public string _filename;
+
+    public List<Entry> _entries = new List<Entry>();
+
+
+    public void SaveToFile()
+    {
+        //code here
+
+    }
+
+    public void LoadFromFile()
+    {
+        //code here
+
+
+    }
+    
+    
+}
