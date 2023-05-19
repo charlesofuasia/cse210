6cb012d697ad229a86fd65da141b3592f6634305
Charles Ofuasia <ofu23001@byui.edu>
2023-05-19 11:57:53 +0100

    No actual changes made.

--- prepare/Learning03/Fraction.cs
+++ prepare/Learning03/Fraction.cs
@@ -45,9 +45,7 @@ public class Fraction
 
     public double GetDecimalValue()
     {
-        return (double)_top / (double)_bottom;
-        
-        
+        return (double)_top / (double)_bottom;  
     }
 
 }
