1c6d750d8bb23aaaa86ae3a8733f75946b1d23a1
Charles Ofuasia <ofu23001@byui.edu>
2023-05-22 09:44:45 +0100

    Reference Class file created

---
+++ prove/Develop03/Reference.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Reference
+{
+    //Reference class attributes
+
+    private string _book;
+    private int _chapter;
+    private int _startVerse;
+    private int _endVerse;
+
+}
