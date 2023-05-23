bcbbc768a727d264077c75f70a561361f428e498
Charles Ofuasia <ofu23001@byui.edu>
2023-05-23 21:56:03 +0100

    Reference class completed and tested

--- prove/Develop03/Reference.cs
+++ prove/Develop03/Reference.cs
@@ -2,11 +2,43 @@ using System;
 
 public class Reference
 {
-    //Reference class attributes
-
+    //Reference class attttributes
     private string _book;
-    private int _chapter;
-    private int _startVerse;
-    private int _endVerse;
+    private string _chapter;
+    private string _startVerse;
+    private string _endVerse;
 
+    //The constructors
+    public Reference(string book, string chapter, string verse)
+    {
+        _book = book;
+        _chapter = chapter;
+        _startVerse = verse;
+        _endVerse = null;
+    }
+
+    public Reference(string book, string chapter, string startVerse, string endVerse)
+    {
+        _book = book;
+        _chapter = chapter;
+        _startVerse = startVerse;
+        _endVerse = endVerse;
+    }
+    //the sole task of the Reference class is to display the reference of the text being memorized
+    //the only method serves a display function
+    public void GetReference()
+    {
+        if (_endVerse == null)
+        {
+             Console.WriteLine($"\n{_book} {_chapter}: {_startVerse}");
+        }
+           
+        else
+        {
+            Console.WriteLine($"\n{_book} {_chapter}: {_startVerse} - {_endVerse}");
+        }
+    
+            
+    }
+   
 }
