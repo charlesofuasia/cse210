c1b30ffc6dca9a342aaeb5983e6cce0500bcd121
Charles Ofuasia <ofu23001@byui.edu>
2023-05-23 20:41:09 +0100

    Edited and completed Word class

---
+++ prove/Develop03/Word.cs
@@ -0,0 +1,42 @@
+using System;
+
+public class Word
+{
+    //Attributes for the Word Class
+    private string _visibleWord;
+    private bool _isHidden;
+
+    //Word class constructor
+    public Word(string word)
+    {
+        _visibleWord = word;
+        _isHidden = false;
+    }
+
+    public void HideWord()
+    {
+        _isHidden = true;
+    }
+
+    public void DisplayWord()
+    {
+        if (_isHidden)
+        {
+            foreach(char letter in _visibleWord)
+            {
+                Console.Write("_");
+            }
+        }
+        else
+            Console.Write(_visibleWord);
+    }
+
+    public bool GetIsHidden()
+    {
+        return _isHidden;
+    }
+
+}
+
+
+
