7c0734e891bbb6a7b637e56889d2ee2c6a5c6113
Charles Ofuasia <ofu23001@byui.edu>
2023-05-24 08:54:35 +0100

    Tested the classes in Program.cs

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        Random rnd = new Random();
+
+        Reference myRef = new Reference("Genesis", "1", "1");
+        Scripture scripture  = new Scripture(myRef, "In the beginning God created the heaven and the earth.");
+
+        bool Display()
+        {
+            Console.Clear();
+            scripture.DisplayScripture();
+            if(scripture.AllHidden())
+                return false;
+            Console.WriteLine("\nType 'Quit' to end program or press enter to continue.");
+            string input = Console.ReadLine();
+            if(input == "Quit")
+                return false;
+            else
+                return true;
+        }
+
+        while(Display())
+        {
+            int counter = 0;
+            while(counter < 3)
+            {
+                int index = rnd.Next(scripture.GetWords().Count);
+                if(!scripture.GetWords()[index].GetIsHidden())
+                {
+                    scripture.GetWords()[index].HideWord();
+                    counter++;
+                }
+
+                if(scripture.AllHidden())
+                    break;
+            }
+        }
     }
 }
