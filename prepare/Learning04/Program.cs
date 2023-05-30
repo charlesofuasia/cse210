66936780b60d1f19a4838494e2ceac0b6c41eb34
Charles Ofuasia <ofu23001@byui.edu>
2023-05-30 12:00:54 +0100

    sub classes loaded to main and tested

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -6,5 +6,20 @@ class Program
     {
         Assignment assignment = new Assignment("Samuel Bennet", "Multiplication");
         Console.WriteLine(assignment.GetSummary());
+        Console.WriteLine();
+
+        MathAssignment maths = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8 - 19");
+        string summary = maths.GetSummary();
+        string problem = maths.GetHomeworkList();
+        Console.WriteLine(summary);
+        Console.WriteLine(problem);
+        Console.WriteLine();
+
+        string title = "The Causes of World War II";
+
+        WritingAssignment writing = new WritingAssignment("Mary Waters", "European History", title);
+        Console.WriteLine(writing.GetSummary());
+        Console.WriteLine(writing.GetWritingInformation());
+
     }
 }
