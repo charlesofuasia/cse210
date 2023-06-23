dee670a0b990c831cd14eda39e202ce54ab371cd
Charles Ofuasia <ofu23001@byui.edu>
2023-06-23 18:58:50 +0100

    Finished writing codes for Simple Goal  sub class

--- prove/Develop05/SimpleGoals.cs
+++ prove/Develop05/SimpleGoals.cs
@@ -4,17 +4,23 @@ public class SimpleGoal : Goal
 {
     public SimpleGoal(string name, string description, int points, bool complete) :base(name, description, points, complete)
     {
-
+        _goalType = "Simple Goal";
     }
 
     public override int RecordEvent()
     { 
         _goalCompletion = true;
+        Console.WriteLine($"Congratulation for completing your goal! You just earned {_goalPoints} points");
         return _goalPoints;
     }
 
     public override string FormatDisplay()
     {
-        return $"{_goalName} - {_goalDescription} - {_goalPoints}";
+        return $"{_goalName} ( {_goalDescription} ) - {_goalPoints}";
+    }
+
+    public override string FormatSave()
+    {
+        return $"{_goalType}|{_goalName}|{_goalDescription}|{_goalPoints}|{_goalCompletion}";
     }
 }
